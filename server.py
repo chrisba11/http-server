50c1dcfe8e15f6879f7e3e844dc6b2c346855b75
Chris Ball <chrisball7@gmail.com>
2019-02-26 14:18:48 -0800

    set up cow route, realized we don't need /test, so about to delete it

--- server.py
+++ server.py
@@ -1,18 +1,43 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+import os
 
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('content-type', 'text/html')
+        parsed_path = urlparse(self.path)
+        parsed_qs = parse_qs(parsed_path.query)
+
+        if parsed_path.path == '/':
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(f'<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cow">Click Here for Fun Stuffs</a></li></ul></nav><header><main>project description</main></body></html>'.encode())
+            return
+        elif parsed_path.path == '/test':
+            try:
+                cat = parsed_qs['category'][0]
+                # print(parsed_qs['category'])
+            except KeyError:
+                self.send_response_only(400)
+                self.end_headers()
+                return
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(f'<html><body><h1>{cat}</h1></body></html>'.encode())
+            return
+        elif parsed_path.path == '/cow':
+            try:
+                cowsay = parsed_qs['category'][0]
+            except KeyError:
+                self.send_response_only(400)
+                self.end_headers()
+                return
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(f'<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cow">{cowsay}</a></li></ul></nav><header><main>project description</main></body></html>'.encode())
+        self.send_response_only(404)
         self.end_headers()
-
-        if self.path == '/':
-            response_text = 'home route'
-        else:
-            response_text = 'another route'
-
-        self.wfile.write(f'<html><body><h1>{response_text}</h1></body></html>'.encode())
 
     def do_POST(self):
         self.send_response(200)
