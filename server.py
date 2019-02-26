daeff091740a70b3b04f930ba363d393f5a0f188
Chris Ball <chrisball7@gmail.com>
2019-02-26 15:09:32 -0800

    got it printing properly in terminal with query string in moose's head

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
+from cowpy import cow
 import os
 
 
@@ -7,35 +8,33 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         parsed_qs = parse_qs(parsed_path.query)
+        print(parsed_qs)
+
+        message = parsed_qs['msg'][0]
+
+        print('this is the message only' + parsed_qs['msg'][0])
 
         if parsed_path.path == '/':
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(f'<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cow">Click Here for Fun Stuffs</a></li></ul></nav><header><main>project description</main></body></html>'.encode())
-            return
-        elif parsed_path.path == '/test':
-            try:
-                cat = parsed_qs['category'][0]
-                # print(parsed_qs['category'])
-            except KeyError:
-                self.send_response_only(400)
-                self.end_headers()
-                return
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(f'<html><body><h1>{cat}</h1></body></html>'.encode())
+
+            koala = cow.Moose()
+            msg = koala.milk(message)
+            print(msg)
+
+            self.wfile.write(f'<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cow">Click Here for Fun Stuffs</a></li></ul></nav><header><main>{msg}</main></body></html>'.encode())
             return
         elif parsed_path.path == '/cow':
             try:
-                cowsay = parsed_qs['category'][0]
+                user_text = parsed_qs['category'][0]
             except KeyError:
                 self.send_response_only(400)
                 self.end_headers()
                 return
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(f'<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cow">{cowsay}</a></li></ul></nav><header><main>project description</main></body></html>'.encode())
+            self.wfile.write(f'<!DOCTYPE html><html><head><title> {user_text} </title></head><body><header><h1>Welcome to Cow Pie Central!</h1><header><main><h2>Instructions:</h2><p></p></main></body></html>'.encode())
         self.send_response_only(404)
         self.end_headers()
 
