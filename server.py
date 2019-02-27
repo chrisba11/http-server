032e8d9eb3cbffc6a01ebe5436f2e2a6117d26dd
Chris Ball <chrisball7@gmail.com>
2019-02-26 16:03:41 -0800

    we have separate animals rendering for each route with the query route displaying the user's input in the bubble

--- server.py
+++ server.py
@@ -8,35 +8,44 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         parsed_qs = parse_qs(parsed_path.query)
-        print(parsed_qs)
 
-        message = parsed_qs['msg'][0]
+        link = '<a href="/cow">Click Here for Fun Stuffs</a>'
 
-        print('this is the message only' + parsed_qs['msg'][0])
+        # print('this is the message only' + parsed_qs['msg'][0])
 
         if parsed_path.path == '/':
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
 
-            koala = cow.Moose()
-            msg = koala.milk(message)
+            animal = cow.Stegosaurus()
+            msg = animal.milk(link)
             print(msg)
 
-            self.wfile.write(f'<!DOCTYPE html><html><head><title> cowsay </title></head><body><header><nav><ul><li><a href="/cow">Click Here for Fun Stuffs</a></li></ul></nav><header><main>{msg}</main></body></html>'.encode())
+            self.wfile.write(f'<!DOCTYPE html><html><head><title>Click Me</title></head><body><header></header><main style="font-family:monospace; font-weight:bold; color:green;"><pre>{msg}</pre></main></body></html>'.encode())
             return
         elif parsed_path.path == '/cow':
-            try:
-                user_text = parsed_qs['category'][0]
-            except KeyError:
-                self.send_response_only(400)
-                self.end_headers()
-                return
+            if parsed_qs:
+                message = parsed_qs['msg'][0]
+                animal = cow.Turkey()
+                msg = animal.milk(message)
+            else:
+                message = "Isn't this fun!? Wanna try?"
+                animal = cow.Tux()
+                msg = animal.milk(message)
+
+            # try:
+            #     user_text = parsed_qs['category'][0]
+            # except KeyError:
+            #     self.send_response_only(400)
+            #     self.end_headers()
+            #     return
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(f'<!DOCTYPE html><html><head><title> {user_text} </title></head><body><header><h1>Welcome to Cow Pie Central!</h1><header><main><h2>Instructions:</h2><p></p></main></body></html>'.encode())
-        self.send_response_only(404)
-        self.end_headers()
+            self.wfile.write(f'<!DOCTYPE html><html><head><title>Sample</title></head><body><header><header><main font-family=monospace><pre>{msg}</pre></main></body></html>'.encode())
+        else:
+            self.send_response_only(404)
+            self.end_headers()
 
     def do_POST(self):
         self.send_response(200)
